52ed632af3b1e7dff7a8c0595bfa179d567af689
Fekim <81150979+97Fekim@users.noreply.github.com>
2024-01-06 19:10:41 +0900

    엔티티 매핑 작업 테스트 코드 작성

--- src/test/java/com/fekim/workweout/online/EntityMappingTest.java
+++ src/test/java/com/fekim/workweout/online/EntityMappingTest.java
@@ -1,13 +1,22 @@
 package com.fekim.workweout.online;
 
+import com.fekim.workweout.online.date.repository.DateRepository;
+import com.fekim.workweout.online.date.repository.entity.Date;
+import com.fekim.workweout.online.date.repository.entity.key.YyyyMmDd;
+import com.fekim.workweout.online.date.repository.entity.key.YyyyMmW;
 import com.fekim.workweout.online.group.repository.GrpRepository;
 import com.fekim.workweout.online.group.repository.MemberGrpRepository;
 import com.fekim.workweout.online.group.repository.entity.Grp;
 import com.fekim.workweout.online.group.repository.entity.MemberGrp;
+import com.fekim.workweout.online.jnal.repository.WkoutJnalMethodRepository;
+import com.fekim.workweout.online.jnal.repository.WkoutJnalRepository;
 import com.fekim.workweout.online.jnal.repository.WkoutMethodRepository;
+import com.fekim.workweout.online.jnal.repository.entity.WkoutJnal;
+import com.fekim.workweout.online.jnal.repository.entity.WkoutJnalMethod;
 import com.fekim.workweout.online.jnal.repository.entity.WkoutMethod;
 import com.fekim.workweout.online.member.repository.MemberRepository;
 import com.fekim.workweout.online.member.repository.entity.Member;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,6 +25,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class EntityMappingTest {
 
     @Autowired
+    private DateRepository dateRepository;
+    @Autowired
     private MemberRepository memberRepository;
     @Autowired
     private GrpRepository grpRepository;
@@ -23,6 +34,22 @@ public class EntityMappingTest {
     private WkoutMethodRepository wkoutMethodRepository;
     @Autowired
     private MemberGrpRepository memberGrpRepository;
+    @Autowired
+    private WkoutJnalRepository wkoutJnalRepository;
+    @Autowired
+    private WkoutJnalMethodRepository wkoutJnalMethodRepository;
+
+    @Test
+    void dateInsertPass() {
+        dateRepository.save(
+                Date.builder()
+                        .yyyyMmDd(YyyyMmDd.builder().yyyy("1111").mm("11").dd("11").build())
+                        .yyyyMmW(YyyyMmW.builder().cuofYyyy("1111").cuofMm("11").cuofWeek("2").build())
+                        .dayOfWeekClsfCd("mon")
+                        .holyDayYn("N")
+                        .build()
+        );
+    }
 
     @Test
     void memberInsertFail() {
@@ -69,6 +96,50 @@ public class EntityMappingTest {
                 WkoutMethod.builder()
                         .methodNm("벤치프레스")
                         .targetPart("가슴")
+                        .build()
+        );
+    }
+
+    @Test
+    void wkoutJnalInsertFail() {
+
+        wkoutJnalRepository.save(
+                WkoutJnal.builder()
+                        .member(Member.builder().mbrId(999L).build())
+                        .yyyyMmDd(YyyyMmDd.builder().yyyy("1111").mm("11").dd("11").build())
+                        .comments("comment")
+                        .build()
+        );
+    }
+
+    @Test
+    void wkoutJnalInsertPass() {
+
+        wkoutJnalRepository.save(
+                WkoutJnal.builder()
+                        .member(Member.builder().mbrId(1L).build())
+                        .yyyyMmDd(YyyyMmDd.builder().yyyy("1111").mm("11").dd("11").build())
+                        .comments("comment")
+                        .build()
+        );
+    }
+
+    @Test
+    void wkoutJnalMethodInsertFail() {
+        wkoutJnalMethodRepository.save(
+                WkoutJnalMethod.builder()
+                        .wkoutJnal(WkoutJnal.builder().jnalId(999L).build())
+                        .wkoutMethod(WkoutMethod.builder().methodId(999L).build())
+                        .build()
+        );
+    }
+
+    @Test
+    void wkoutJnalMethodInsertPass() {
+        wkoutJnalMethodRepository.save(
+                WkoutJnalMethod.builder()
+                        .wkoutJnal(WkoutJnal.builder().jnalId(3L).build())
+                        .wkoutMethod(WkoutMethod.builder().methodId(1L).build())
                         .build()
         );
     }
