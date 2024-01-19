0871d9aaa42ccc92bcc4e8af70e6b2c48fedd96a
97Fekim <rlatmdcjf9@gmail.com>
2024-01-19 14:38:58 +0900

    REFACTOR: 로그 클리어

--- src/main/java/com/fekim/workweout/online/member/controller/MemberController.java
+++ src/main/java/com/fekim/workweout/online/member/controller/MemberController.java
@@ -4,6 +4,7 @@ import com.fekim.workweout.online.member.repository.entity.Member;
 import com.fekim.workweout.online.member.service.MemberService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,12 +18,12 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping("/member/login")
-    public ResponseEntity<String> login(HttpSession session, String email, String password) throws AuthenticationException {
+    public ResponseEntity<String> login(HttpSession session, String email, String password) {
 
         try {
             memberService.login(session, email, password);
         } catch (AuthenticationException e) {
-            return ResponseEntity.internalServerError().build();
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
 
         return ResponseEntity.ok("LOGIN OK");
