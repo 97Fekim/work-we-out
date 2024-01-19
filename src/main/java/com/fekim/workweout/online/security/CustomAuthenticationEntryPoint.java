e3f89416a5c66a4e35ef234a53fece2c646eb2d5
Fekim <81150979+97Fekim@users.noreply.github.com>
2024-01-19 23:12:53 +0900

    [인증/인가] 인증되지 않은 유저의 접근은 리다이렉트 처리

--- src/main/java/com/fekim/workweout/online/security/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/fekim/workweout/online/security/CustomAuthenticationEntryPoint.java
@@ -3,8 +3,11 @@ package com.fekim.workweout.online.security;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -17,10 +20,7 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
 
         System.out.println("[DEBUG]===================CustomAuthenticationEntryPoint Start==============");
 
-        response.setContentType("text/plain;charset=UTF-8");
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        response.getWriter().flush();
-        response.getWriter().close();
-
+        // 인증 실패시 로그인화면으로 리다이렉트한다.
+        response.sendRedirect("/signIn");
     }
 }
