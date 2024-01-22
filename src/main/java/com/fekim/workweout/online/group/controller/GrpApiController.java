f8fb365d730609ea7da1c72950845a31b6b2863d
97Fekim <rlatmdcjf9@gmail.com>
2024-01-22 17:12:58 +0900

    [API-S].1 CSR 샘플 작성

--- src/main/java/com/fekim/workweout/online/group/controller/GrpApiController.java
+++ src/main/java/com/fekim/workweout/online/group/controller/GrpApiController.java
@@ -1,2 +1,35 @@
-package com.fekim.workweout.online.group.controller;public class GrpApiController {
+package com.fekim.workweout.online.group.controller;
+
+import com.fekim.workweout.online.group.service.GrpService;
+import com.fekim.workweout.online.group.service.dto.GrpListDTO;
+import com.fekim.workweout.online.member.repository.entity.Member;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Log4j2
+@RestController
+@RequestMapping("/grp/")
+@RequiredArgsConstructor
+public class GrpApiController {
+
+    private final GrpService grpService;
+
+    @GetMapping("/myGrps")
+    ResponseEntity<GrpListDTO> getMyGrps(HttpSession session) {
+
+        Member member = (Member) session.getAttribute("LOGIN_MEMBER");
+        Long mbrId = member.getMbrId();
+
+        GrpListDTO grpListDTO = grpService.getGrpListByMbrId(mbrId);
+
+        return new ResponseEntity<>(grpListDTO, HttpStatus.OK);
+    }
+
+
 }
--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -18,53 +18,33 @@ $(".toggle-side-bar-btn").click(function () {
     /* 아이콘에 active 클래스가 없으면 active 클래스를 넣어주고 있으면 빼줌 */
     $clicked.toggleClass("active");
 });
-  
+
 /* 왼쪽 사이드바 함수 */
 function showLeftSideBar() {
-    /* 메뉴바가 나올때 안에 펼쳐져 있는 메뉴들을 다 접기위해 엑티브를 없앤다 */
-    $(".left-side-bar > .menu-1 ul > li.active").removeClass("active");
+    $(".left-side-bar > .menu-1 ul > li").addClass("active");
     $(".left-side-bar-box").addClass("active");
 }
 function hideLeftSideBar() {
     $(".left-side-bar-box").removeClass("active");
 }
 
-/* 메뉴 접히고 펼치기 */
-$(".left-side-bar > .menu-1 ul > li").click(function (e) {
-
-    /* 만약 클릭된 메뉴에 엑티브 클래스가 있으면 */
-    if ($(this).hasClass("active")) {
-        /* 클릭된 메뉴의 엑티브를 없앤다 */
-        $(this).removeClass("active");
-    } else {
-        /* 클릭된 메뉴(지역)의 엑티브를 없앤다 */
-        $(this).find(".active").removeClass("active");
-
-        /* 클릭된 메뉴의 엑티브를 만든다 */
-        $(this).addClass("active");
-    }
-
-    /* 클릭된 메뉴 안에 다른 메뉴를 클릭하면 위에있는 메뉴가 같이 클릭되는데 그것을 막아준다 */
-    e.stopPropagation();
-});
-  
 /* 좌측 사이드바 배경을 클릭했을때 */
 $(".left-side-bar-box").click(function () {
 
     /* 토글 사이드바 버튼을 클릭한 효과를 만듬 */
     $(".toggle-side-bar-btn").click();
 });
-  
+
 /* 사이드바를 클릭할때 상위요소인 배경이 같이 클릭되어서 사이드바가 들어가버리기 때문에 그것을 막음 */
 $(".left-side-bar").click(function (e) {
     e.stopPropagation();
 });
-  
+
 // 사이드바 열릴 때 select 박스 숨기기
 function hideSelectBox() {
     $(".select-box").css("visibility", "hidden");
 }
-  
+
 // 사이드바 닫힐 때 select 박스 보이기
 function showSelectBox() {
     $(".select-box").css("visibility", "visible");
@@ -75,7 +55,7 @@ function showSelectBox() {
 function mainListModalOpen() {
     var modal = document.getElementById("main-list-modal");
     var modalBackground = document.getElementById("main-list-modal-background");
-    
+
     modal.style.display = "block";
     modalBackground.style.display = "flex";
 }
@@ -84,7 +64,7 @@ function mainListModalOpen() {
 function mainListModalClose() {
     var modal = document.getElementById("main-list-modal");
     var modalBackground = document.getElementById("main-list-modal-background");
-    
+
     modal.style.display = "none";
     modalBackground.style.display = "none";
 }
@@ -97,7 +77,7 @@ function newWorkoutGroupModalOpen() {
     modal.style.display = "block";
     modalBackground.style.display = "flex";
 }
-  
+
 // 새그룹 모달 닫기 함수
 function newWorkoutGroupModalClose() {
     var modal = document.getElementById("new-workout-group-modal");
@@ -106,3 +86,4 @@ function newWorkoutGroupModalClose() {
     modal.style.display = "none";
     modalBackground.style.display = "none";
 }
+
