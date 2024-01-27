6d8485b4a62f6f04afe0b4b51abdbc24d7a50650
Fekim <81150979+97Fekim@users.noreply.github.com>
2024-01-28 00:04:29 +0900

    REFACTOR: .js 리소스 참조구조 변경

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -68,6 +68,10 @@ function newWorkoutGroupModalClose() {
     modalBackground.style.display = "none";
 }
 
+// 운동부위 컬러 MAP
+var partColorMap;
+
+// 운동부위 컬러 MAP 생성
 function createPartColorMap() {
     partColorMap = new Map([
         ['가슴', '#c0ed70'],
@@ -77,3 +81,82 @@ function createPartColorMap() {
     ])
     return partColorMap;
 }
+
+/**
+ * SIDEBAR EVENT) 사이드바 내 그룹 렌더링
+ * */
+function renderSideGrps(grpListDTO) {
+    grpListDTO.grpDTOList.forEach(function(grpDTO, idx) {
+        var d1_ul = document.getElementById("my-grps");
+
+        var d2_li = document.createElement("li");
+
+        var d3_a = document.createElement("a");
+        d3_a.setAttribute("href", "#");
+        d3_a.setAttribute("style", "padding-left:20px");
+        d3_a.innerText = "> " + grpDTO.grpNm;
+
+        var d3_ul = document.createElement("ul");
+
+        var d4_li1 = document.createElement("li");
+
+        var d5_a1 = document.createElement("a");
+        d5_a1.setAttribute("href", "/grp/info?grpId="+grpDTO.grpId);
+        d5_a1.setAttribute("style", "padding-left:50px");
+        d5_a1.innerText = "그룹 페이지";
+
+        var d4_li2 = document.createElement("li");
+        var d5_a2 = document.createElement("a");
+        d5_a2.setAttribute("href", "/grp/grpCalendar?grpId="+grpDTO.grpId);
+        d5_a2.setAttribute("style", "padding-left:50px");
+        d5_a2.innerText = "운동 캘린더";
+
+        var d4_li3 = document.createElement("li");
+        var d5_a3 = document.createElement("a");
+        d5_a3.setAttribute("href", "/stat/grp?grpId="+grpDTO.grpId);
+        d5_a3.setAttribute("style", "padding-left:50px");
+        d5_a3.innerText = "운동 통계";
+
+        d4_li1.appendChild(d5_a1);
+        d4_li2.appendChild(d5_a2);
+        d4_li3.appendChild(d5_a3);
+
+        d3_ul.appendChild(d4_li1);
+        d3_ul.appendChild(d4_li2);
+        d3_ul.appendChild(d4_li3);
+
+        d2_li.appendChild(d3_a);
+        d2_li.appendChild(d3_ul);
+
+        d1_ul.appendChild(d2_li);
+
+    });
+}
+
+/**
+ * SIDEBAR EVENT) 신규 그룹 생성
+ * - param = [그룹명]
+ * - response = [신규그룹ID]
+ * - redirect = [그룹정보페이지]
+ */
+$(document).on("click", "#newGrpBtn", function() {
+    var newGrpNm = document.getElementById("newGrpNm").value;
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:8080/grp/register",
+        dataType:'json',
+        data: {
+            grpNm : newGrpNm
+        },
+        success: function (response) {
+            alert("새 그룹이 생성되었습니다.");
+            location.href="/grp/info?grpId="+response;
+        },
+        error: function (request, error) {
+            console.log("그룹 생성 도중 오류가 발생하였습니다.");
+            alert("code:"+request.status+"\n"
+                +"message:"+request.responseText+"\n"
+                +"error:"+error)
+        }
+    })
+});
