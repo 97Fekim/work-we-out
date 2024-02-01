eb43a448c622304afb9abcc7eb3a58f563d34621
97Fekim <rlatmdcjf9@gmail.com>
2024-02-01 13:13:14 +0900

    [UI].7 그룹정보조회 개발

---
+++ src/main/java/com/fekim/workweout/online/group/controller/GrpController.java
@@ -0,0 +1,35 @@
+package com.fekim.workweout.online.group.controller;
+
+import com.fekim.workweout.online.group.service.GrpService;
+import com.fekim.workweout.online.group.service.dto.GrpDTO;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * 그룹 서비스 MVC 컨트롤러
+ * */
+@Log4j2
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/grp")
+public class GrpController {
+
+    private final GrpService grpService;
+
+    @GetMapping("/grp-info")
+    public void grpInfo(@RequestParam("grpId") Long grpId,
+                             Model model) {
+
+        GrpDTO grpDTO = grpService.getGrpInfo(grpId);
+
+        model.addAttribute("grpDTO", grpDTO);
+
+    }
+
+}
